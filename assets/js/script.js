1595fe10922e784181f8f8a02a722873dd5d53ad
teachergg <jwhitney@bisd.us>
2022-03-18 10:59:34 -0500

    able to show event array in console log through showEvents function

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,61 +6,64 @@ var cityInputEl = document.querySelector("#city-name");
 
 // Dynamically add to DOM Weather Information
 
-// Dynamically add to DOM Event List
 
-// set variables for specific data  "var eventInfo = _embedded.events"
+// "https://app.ticketmaster.com/discovery/v2/events.json?&apikey=bQAmmn8y0TYxPWEysqGkzSsdLE6iLGOx&city="+city+"&sort=date%2Cname%2Casc&page=0&size=5";
 
 function getEvents(city) {
-  var apiUrl = "https://app.ticketmaster.com/discovery/v2/events.json?&apikey=bQAmmn8y0TYxPWEysqGkzSsdLE6iLGOx&city="+city+"&sort=date%2Cname%2Casc&page=0&size=5";
-
-  fetch(apiUrl).then(function(response){
-    if (response.ok) {
-      response.json().then(function(data){
-        console.log(data);
-      });
-    } else {
-      $("#event-list").text("<p class='title'>Error: City Not Found</p>");
-    }
-  })
+  $.ajax({
+    type:"GET",
+    url:"https://app.ticketmaster.com/discovery/v2/events.json?&apikey=bQAmmn8y0TYxPWEysqGkzSsdLE6iLGOx&city="+city+"&sort=date%2Cname%2Casc&page=0&size=5",
+    async:true,
+    dataType: "json",
+    success: function(json) {
+          getEvents.json = json;
+  			  showEvents(json);
+  		   },
+    error: function(xhr, status, err) {
+  			  console.log(err);
+  		   }
+  });
 };
 
 var formSubmitHandler = function(event) {
   event.preventDefault();
   // get city name
   var cityname = cityInputEl.value.trim();
-  console.log(cityname)
   if (cityname) {
     getEvents(cityname);
   } else {
     eventContainerEl.innerHTML = "<p class='title'>City Not Found, Please Enter Valid City Name</p>"
   };
+};
+
+// Dynamically add to DOM Event List
+
+function showEvents(json) {
+  var items = $('#event-list .column');
+  items.hide();
+  var events = json._embedded.events;
+  console.log(events)
+  var item = items.first();
+  for (var i=0;i<events.length;i++) {
+    item.children('#event-title').text(events[i].name);
+    item.children('#event-info').text(events[i].dates.start.localDate);
+    try {
+      item.children('#event-venue').text(events[i]._embedded.venues[0].name + " in " + events[i]._embedded.venues[0].city.name);
+    } catch (err) {
+      console.log(err);
+    }
+    item.show();
+    item.off("click");
+    item.click(events[i], function(eventObject) {
+      console.log(eventObject.data);
+      try {
+        getAttraction(eventObject.data._embedded.attractions[0].id);
+      } catch (err) {
+      console.log(err);
+      }
+    });
+    item=item.next();
+  }
 }
-// function showEvents(data) {
-//   var items = $('#event-list .column');
-//   items.hide();
-//   var events = _embedded.events;
-//   console.log(events)
-//   var item = items.first();
-  // for (var i=0;i<events.length;i++) {
-  //   item.children('#event-title').text(events[i].name);
-  //   item.children('#event-info').text(events[i].dates.start.localDate);
-  //   try {
-  //     item.children('#event-venue').text(events[i]._embedded.venues[0].name + " in " + events[i]._embedded.venues[0].city.name);
-  //   } catch (err) {
-  //     console.log(err);
-  //   }
-  //   item.show();
-  //   item.off("click");
-  //   item.click(events[i], function(eventObject) {
-  //     console.log(eventObject.data);
-  //     try {
-  //       getAttraction(eventObject.data._embedded.attractions[0].id);
-  //     } catch (err) {
-  //     console.log(err);
-  //     }
-  //   });
-  //   item=item.next();
-  // }
-// }
 
 userFormEl.addEventListener("submit", formSubmitHandler);
