95909b24ed077ef986ebe680cd39f1acafbb4e65
teachergg <jwhitney@bisd.us>
2022-03-18 16:31:41 -0500

    added showing results in city name to event list

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,10 @@
 var userFormEl = document.querySelector("#user-form");
 var submitButtonEl = document.querySelector("#search-btn");
 var eventContainerEl = document.querySelector("#event-list");
+var eventCityEl = document.querySelector("#city-search")
 var cityInputEl = document.querySelector("#city-name");
 var dateInputEl = document.querySelector("#datepicker")
-var eventContainerEl = $('#event-list');
+
 
 $(function() {
   $("#datepicker").datepicker({
@@ -15,11 +16,6 @@ $(function() {
 // Get Weather API
 
 // Dynamically add to DOM Weather Information
-
-var dataArr = [1, 2, 3, 4, 5];
-
-// "https://app.ticketmaster.com/discovery/v2/events.json?&apikey=bQAmmn8y0TYxPWEysqGkzSsdLE6iLGOx&city="+city+"&sort=date%2Cname%2Casc&page=0&size=5";
-
 function getEvents(city, startdate) {
   $.ajax({
     type:"GET",
@@ -44,19 +40,17 @@ var formSubmitHandler = function(event) {
   if (cityname, getStartDate) {
     getEvents(cityname, getStartDate);
   } else {
-    eventContainerEl.innerHTML = "<p class='title'>City Not Found, Please Enter Valid City Name</p>"
+    console.log("City Not Found, Please Enter Valid City Name");
   };
 };
 
 
 // Dynamically add to DOM Event List
-
-// events[i].name
-// events[i].dates.start.localDate
-// events[i]._embedded.venues[0].name
-
 function showEvents(json) {
   var events = json._embedded.events;
+  var eventListCity = cityInputEl.value.trim();
+  eventCityEl.textContent = "Showing Results for: "+eventListCity+".";
+
 
   for (var i=0;i< 5; i++) {
     // event title 
@@ -80,7 +74,8 @@ function showEvents(json) {
     eventContainerEl.append(eventTitleEl);
     eventContainerEl.append(eventDateEl);
     eventContainerEl.append(eventVenueEl);
-  }
+  };
+
 
 }
 
