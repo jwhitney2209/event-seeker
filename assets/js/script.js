9da395c9279db6fe679cde08807c7838f8ce91d6
teachergg <jwhitney@bisd.us>
2022-03-18 10:45:32 -0500

    fixed form submission to export user input

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,7 @@
 var userFormEl = document.querySelector("#user-form")
 var submitButtonEl = document.querySelector("#search-btn")
-var cityInputEl = document.querySelector("#city-name");
 var eventContainerEl = document.querySelector("#event-list")
-
+var cityInputEl = document.querySelector("#city-name");
 // Get Weather API
 
 // Dynamically add to DOM Weather Information
@@ -11,57 +10,57 @@ var eventContainerEl = document.querySelector("#event-list")
 
 // set variables for specific data  "var eventInfo = _embedded.events"
 
-function getEvents(page) {
+// function getEvents(city) {
+//   var apiUrl = "https://app.ticketmaster.com/discovery/v2/events.json?&apikey=bQAmmn8y0TYxPWEysqGkzSsdLE6iLGOx&city="+city+"&sort=date%2Cname%2Casc&page=0&size=5";
 
-  $('#events-panel').show();
-  $('#attraction-panel').hide();
-
-  if (page < 0) {
-    page = 0;
-    return;
-  }
-  if (page > 0) {
-    if (page > getEvents.json.page.totalPages-1) {
-      page=0;
-    }
-  }
-  
-  $.ajax({
-    type:"GET",
-    url:"https://app.ticketmaster.com/discovery/v2/events.json?apikey=bQAmmn8y0TYxPWEysqGkzSsdLE6iLGOx&size=5&page="+page,
-    async:true,
-    dataType: "json",
-    success: function(json) {
-          getEvents.json = json;
-  			  showEvents(json);
-  		   },
-    error: function(xhr, status, err) {
-  			  console.log(err);
-  		   }
-  });
-}
-
-// Handle Form Submission
+//   fetch(apiUrl).then(function(response){
+//     if (response.ok) {
+//       response.json().then(function(data){
+//         console.log(data);
+//       });
+//     } else {
+//       $("#event-list").text("<p class='title'>Error: City Not Found</p>");
+//     }
+//   })
+// };
 
 var formSubmitHandler = function(event) {
   event.preventDefault();
-
-  // get user inpout
-  var cityName = cityInputEl.value.trim();
-
-  if (cityName) {
-    getLocationEvents(cityName);
-    cityInputEl.value ="";
+  // get city name
+  var cityname = cityInputEl.value.trim();
+  console.log(cityname)
+  if (cityname) {
+    getEvents(cityname);
   } else {
-    // replace this alert with a function that changes the dom with a connect error message
-    alert("Please Enter a City");
-  }
-};
+    eventContainerEl.innerHTML = "<p class='title'>City Not Found, Please Enter Valid City Name</p>"
+  };
+}
+// function showEvents(data) {
+//   var items = $('#event-list .column');
+//   items.hide();
+//   var events = _embedded.events;
+//   console.log(events)
+//   var item = items.first();
+  // for (var i=0;i<events.length;i++) {
+  //   item.children('#event-title').text(events[i].name);
+  //   item.children('#event-info').text(events[i].dates.start.localDate);
+  //   try {
+  //     item.children('#event-venue').text(events[i]._embedded.venues[0].name + " in " + events[i]._embedded.venues[0].city.name);
+  //   } catch (err) {
+  //     console.log(err);
+  //   }
+  //   item.show();
+  //   item.off("click");
+  //   item.click(events[i], function(eventObject) {
+  //     console.log(eventObject.data);
+  //     try {
+  //       getAttraction(eventObject.data._embedded.attractions[0].id);
+  //     } catch (err) {
+  //     console.log(err);
+  //     }
+  //   });
+  //   item=item.next();
+  // }
+// }
 
-
-var displayEvents = function(){
-
-};
-
-submitButtonEl.addEventListener("submit", formSubmitHandler);
-getLocationEvents();
+userFormEl.addEventListener("submit", formSubmitHandler);
